1a4ebeddfc14372a9cb54301f0f8af44bd7644cd
Dipendra <dipendrabhandari0000@gmail.com>
2023-12-03 23:09:00 +0545

    allblogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AllBlogs from './pages/allblogs/allblogs';
+import AllBlogs from './pages/AllBlogs/AllBlogs'
 import CreateBlogs from './pages/CreateBlogs/CreateBlogs';
 import EditBlogs from './pages/EditBlogs/EditBlogs';
 import SingleBlogs from './pages/SingleBlogs/SingleBlogs';
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<AllBlogs />} />
+      <Route path='/' element={< AllBlogs />} />
         <Route path='/CreateBlogs' element={<CreateBlogs />} />
         <Route path='/EditBlogs/:id' element={<EditBlogs />} />
         <Route path='/SingleBlogs/:id' element={<SingleBlogs />} />
