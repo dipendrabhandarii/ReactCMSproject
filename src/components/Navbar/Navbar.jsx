d234afa07d975ff5f238979a14e0a7f43eccdbae
Dipendra <dipendrabhandari0000@gmail.com>
2023-12-03 21:29:03 +0545

    css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,16 +3,11 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li >
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/Createblogs">Add Blog</a>
-        </li>
-      </ul>
-    </nav>
+    <div> <nav>
+    <a href="/">Home</a>
+    <a href="/Createblogs">Add Blog</a>
+  </nav></div>
+   
   );
 };
 
