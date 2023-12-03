7026a23c6adc6cc8a0d47d632a4f24b65d40fd61
Dipendra <dipendrabhandari0000@gmail.com>
2023-12-03 23:01:32 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import AllBlogs from './pages/allblogs/allblogs'
-import CreateBlogs from './pages/CreateBlogs/CreateBlogs'
-import EditBlogs from './pages/EditBlogs/EditBlogs'
-import SingleBlogs from './pages/SingleBlogs/SingleBlogs'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AllBlogs from './pages/allblogs/allblogs';
+import CreateBlogs from './pages/CreateBlogs/CreateBlogs';
+import EditBlogs from './pages/EditBlogs/EditBlogs';
+import SingleBlogs from './pages/SingleBlogs/SingleBlogs';
 
 const App = () => {
   return (
-   
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<AllBlogs />} />
-      <Route path='/CreateBlogs' element={<CreateBlogs/>} />
-      <Route path='/EditBlogs/:id' element={<EditBlogs />} />
-      <Route path='SingleBlogs/:id' element={<SingleBlogs />} />
-    </Routes>
-    
+      <Routes>
+        <Route path='/' element={<AllBlogs />} />
+        <Route path='/CreateBlogs' element={<CreateBlogs />} />
+        <Route path='/EditBlogs/:id' element={<EditBlogs />} />
+        <Route path='/SingleBlogs/:id' element={<SingleBlogs />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
