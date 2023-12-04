2cfd4b74a0037e8c0f2a98b564c51c447fcc64de
Dipendra <dipendrabhandari0000@gmail.com>
2023-12-04 20:52:29 +0545

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div> <nav>
-    <a href="/">Home</a>
-    <a href="/CreateBlogs">Add Blog</a>
-  </nav></div>
-   
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/CreateBlogs">Add Blog</Link>
+      </nav>
+    </div>
   );
 };
 
